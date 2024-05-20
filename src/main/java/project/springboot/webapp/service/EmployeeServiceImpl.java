e51401b6076cf0f673f5cdaf1a2f408ffe4781ac
steven <huynhtanhoa2012@gmail.com>
2024-05-20 13:14:28 +0700

    update index page, Employee

--- src/main/java/project/springboot/webapp/service/EmployeeServiceImpl.java
+++ src/main/java/project/springboot/webapp/service/EmployeeServiceImpl.java
@@ -1,11 +1,14 @@
 package project.springboot.webapp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import project.springboot.webapp.model.Employee;
 import project.springboot.webapp.repository.EmployeeRepository;
 
 import java.util.List;
 
+
+@Service
 public class EmployeeServiceImpl implements EmployeeService{
 
     @Autowired
