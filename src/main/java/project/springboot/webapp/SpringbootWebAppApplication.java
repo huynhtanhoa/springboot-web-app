d0fce509b7837ebaa2a662883c38b619289d1677
steven <huynhtanhoa2012@gmail.com>
2024-05-20 10:56:47 +0700

    Initial commit

---
+++ src/main/java/project/springboot/webapp/SpringbootWebAppApplication.java
@@ -0,0 +1,13 @@
+package project.springboot.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/springboot/webapp/SpringbootWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package project.springboot.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
