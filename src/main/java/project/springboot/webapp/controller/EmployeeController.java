ab6a9b9ca5ac8d715bf6acc04de968239acaa533
steven <huynhtanhoa2012@gmail.com>
2024-05-21 14:30:50 +0700

    Implement Update Employee Feature

--- src/main/java/project/springboot/webapp/controller/EmployeeController.java
+++ src/main/java/project/springboot/webapp/controller/EmployeeController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import project.springboot.webapp.model.Employee;
 import project.springboot.webapp.service.EmployeeService;
 
@@ -29,5 +32,23 @@ public class EmployeeController {
         Employee employee = new Employee();
         model.addAttribute("employee", employee);
         return "new_employee";
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        // save employee to database
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
+    }
+
+    @GetMapping("/showFormForUpdate/{id}")
+    public String showFormForUpdate(@PathVariable (value ="id") long id, Model model){
+
+        // get Employee from the service
+        Employee employee = employeeService.getEmployeeById(id);
+
+        // Set Employee as a model attribute to the form
+        model.addAttribute("employee", employee);
+        return "update_employee";
     }
 }
--- src/main/java/project/springboot/webapp/service/EmployeeServiceImpl.java
+++ src/main/java/project/springboot/webapp/service/EmployeeServiceImpl.java
@@ -6,6 +6,7 @@ import project.springboot.webapp.model.Employee;
 import project.springboot.webapp.repository.EmployeeRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -22,5 +23,17 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public void saveEmployee(Employee employee) {
         this.employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee getEmployeeById(long id) {
+        Optional<Employee> optional = employeeRepository.findById(id);
+        Employee employee = null;
+        if(optional.isPresent()){
+            employee = optional.get();
+        } else {
+            throw new RuntimeException("Employee not found for id: " + id);
+        }
+        return employee;
     }
 }
